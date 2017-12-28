3867595296333f59add548ce663675b46c96b10a
libremente <surf@libremente.eu>
2017-12-28 18:32:02 +0100

    Added webpack config JS file

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+var webpack = require("webpack");
+
+module.exports = {
+    entry: './src/fare_python.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module:{
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery',
+            Popper: ['popper.js', 'default'],
+            // In case you imported plugins individually, you must also require them here:
+            Util: "exports-loader?Util!bootstrap/js/dist/util",
+            Dropdown: "exports-loader?Dropdown!bootstrap/js/dist/dropdown",
+      })
+    ],
+};
+
