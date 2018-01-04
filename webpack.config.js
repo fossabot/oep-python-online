739f76b5b16f81d4109a790d0d6f97f7fc9a90a1
libremente <surf@libremente.eu>
2018-01-04 11:41:58 +0100

    Renamed fare_python.js into app.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 var webpack = require("webpack");
 
 module.exports = {
-    entry: './src/fare_python.js',
+    entry: './src/app.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -4,7 +4,7 @@ var webpack = require("webpack");
 
 module.exports = {
     devtool: 'source-map',
-    entry: './src/fare_python.js',
+    entry: './src/app.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
